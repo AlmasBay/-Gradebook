714d870adffd16457ddc4c96ca3a99ba95b8a68c
AlmasBay <108793807+AlmasBay@users.noreply.github.com>
2022-11-19 23:35:41 +0600

    Add Project

---
+++ Almas.List/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace Almas.List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewListOfStudents = new System.Windows.Forms.DataGridView();
+            this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FirstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Lastname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxSeconName = new System.Windows.Forms.TextBox();
+            this.textBoxRating = new System.Windows.Forms.TextBox();
+            this.textBoxAverage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.buttonMin = new System.Windows.Forms.Button();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.buttonMax = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListOfStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewListOfStudents
+            // 
+            this.dataGridViewListOfStudents.AllowUserToAddRows = false;
+            this.dataGridViewListOfStudents.AllowUserToDeleteRows = false;
+            this.dataGridViewListOfStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewListOfStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Number,
+            this.FirstName,
+            this.Lastname,
+            this.Rate});
+            this.dataGridViewListOfStudents.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewListOfStudents.Name = "dataGridViewListOfStudents";
+            this.dataGridViewListOfStudents.ReadOnly = true;
+            this.dataGridViewListOfStudents.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewListOfStudents.Size = new System.Drawing.Size(445, 235);
+            this.dataGridViewListOfStudents.TabIndex = 0;
+            this.dataGridViewListOfStudents.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewListOfStudents_CellClick);
+            // 
+            // Number
+            // 
+            this.Number.HeaderText = "№";
+            this.Number.Name = "Number";
+            this.Number.ReadOnly = true;
+            // 
+            // FirstName
+            // 
+            this.FirstName.HeaderText = "Имя";
+            this.FirstName.Name = "FirstName";
+            this.FirstName.ReadOnly = true;
+            // 
+            // Lastname
+            // 
+            this.Lastname.HeaderText = "Фамилия";
+            this.Lastname.Name = "Lastname";
+            this.Lastname.ReadOnly = true;
+            // 
+            // Rate
+            // 
+            this.Rate.HeaderText = "Оценка";
+            this.Rate.Name = "Rate";
+            this.Rate.ReadOnly = true;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(576, 24);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(160, 20);
+            this.textBoxFirstName.TabIndex = 1;
+            // 
+            // textBoxSeconName
+            // 
+            this.textBoxSeconName.Location = new System.Drawing.Point(576, 68);
+            this.textBoxSeconName.Name = "textBoxSeconName";
+            this.textBoxSeconName.Size = new System.Drawing.Size(160, 20);
+            this.textBoxSeconName.TabIndex = 2;
+            // 
+            // textBoxRating
+            // 
+            this.textBoxRating.Location = new System.Drawing.Point(576, 114);
+            this.textBoxRating.Name = "textBoxRating";
+            this.textBoxRating.Size = new System.Drawing.Size(160, 20);
+            this.textBoxRating.TabIndex = 3;
+            // 
+            // textBoxAverage
+            // 
+            this.textBoxAverage.Location = new System.Drawing.Point(644, 247);
+            this.textBoxAverage.Name = "textBoxAverage";
+            this.textBoxAverage.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAverage.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(522, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(514, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Фамилия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(522, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Оценка";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(525, 156);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(113, 32);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Редактировать";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(644, 156);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 32);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(525, 236);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(113, 40);
+            this.btnAverage.TabIndex = 10;
+            this.btnAverage.Text = "Среднее Арифметическое";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(525, 194);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(113, 36);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Удалить";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // buttonMin
+            // 
+            this.buttonMin.Location = new System.Drawing.Point(525, 281);
+            this.buttonMin.Name = "buttonMin";
+            this.buttonMin.Size = new System.Drawing.Size(113, 42);
+            this.buttonMin.TabIndex = 12;
+            this.buttonMin.Text = " Минимальная\r\nОценка\r\n";
+            this.buttonMin.UseVisualStyleBackColor = true;
+            this.buttonMin.Click += new System.EventHandler(this.buttonMin_Click);
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(644, 293);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMin.TabIndex = 13;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(644, 342);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMax.TabIndex = 16;
+            // 
+            // buttonMax
+            // 
+            this.buttonMax.Location = new System.Drawing.Point(525, 329);
+            this.buttonMax.Name = "buttonMax";
+            this.buttonMax.Size = new System.Drawing.Size(113, 42);
+            this.buttonMax.TabIndex = 17;
+            this.buttonMax.Text = "Максимальная Оценка";
+            this.buttonMax.UseVisualStyleBackColor = true;
+            this.buttonMax.Click += new System.EventHandler(this.buttonMax_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonMax);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.buttonMin);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAverage);
+            this.Controls.Add(this.textBoxRating);
+            this.Controls.Add(this.textBoxSeconName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.dataGridViewListOfStudents);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListOfStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewListOfStudents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FirstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Lastname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rate;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxSeconName;
+        private System.Windows.Forms.TextBox textBoxRating;
+        private System.Windows.Forms.TextBox textBoxAverage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button buttonMin;
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.TextBox textBoxMax;
+        private System.Windows.Forms.Button buttonMax;
+    }
+}
+
---
+++ Almas.List/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Almas.List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+       
+        
+        
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string[] student = new string[4];
+
+            student[0] = (dataGridViewListOfStudents.Rows.Count + 1).ToString();
+            student[1] = textBoxFirstName.Text;
+            student[2] = textBoxSeconName.Text;
+            student[3] = textBoxRating.Text;
+
+            dataGridViewListOfStudents.Rows.Add(student[0], student[1], student[2], student[3]);
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            dataGridViewListOfStudents.Rows.Remove(dataGridViewListOfStudents.SelectedRows[0]);
+
+            textBoxFirstName.Clear();
+            textBoxSeconName.Clear();
+            textBoxRating.Clear();
+
+        }
+
+        
+
+        private void dataGridViewListOfStudents_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxFirstName.Text = dataGridViewListOfStudents.SelectedRows[0].Cells[1].Value.ToString();
+            textBoxSeconName.Text = dataGridViewListOfStudents.SelectedRows[0].Cells[2].Value.ToString();
+            textBoxRating.Text = dataGridViewListOfStudents.SelectedRows[0].Cells[3].Value.ToString();
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            string[] student = new string[3];
+
+            student[0] = textBoxFirstName.Text;
+            student[1] = textBoxSeconName.Text;
+            student[2] = textBoxRating.Text;
+
+            dataGridViewListOfStudents.SelectedRows[0].Cells[1].Value = student[0];
+            dataGridViewListOfStudents.SelectedRows[0].Cells[2].Value = student[1];
+            dataGridViewListOfStudents.SelectedRows[0].Cells[3].Value = student[2];
+
+
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            int summ = 0;
+            int count = dataGridViewListOfStudents.Rows.Count;
+
+            if(count != 0)
+            {
+                for (int i = 0; i  < count; i++)
+                {
+                    int rating = Convert.ToInt32(dataGridViewListOfStudents.Rows[i].Cells[3].Value);
+                    summ += rating;
+
+                }
+                int average = summ/count;
+
+                textBoxAverage.Text = average.ToString();
+            
+            }
+        }
+
+        private void buttonMin_Click(object sender, EventArgs e)
+        {
+            int count = dataGridViewListOfStudents.Rows.Count;
+
+            if(count != 0)
+            {
+                int min = Convert.ToInt32(dataGridViewListOfStudents.Rows[0].Cells[3].Value);
+
+              if (count  > 1)
+                {
+                    for (int i = 1; i < dataGridViewListOfStudents.Rows.Count; i++)
+                    {
+                        int rating = Convert.ToInt32(dataGridViewListOfStudents.Rows[i].Cells[3].Value);
+                        if(min > rating)
+                        {
+                            min = rating;
+                        }
+                    }
+
+
+
+              }
+
+
+                textBoxMin.Text = min.ToString();
+
+
+            }
+
+
+
+
+        }
+
+        private void buttonMax_Click(object sender, EventArgs e)
+        {
+            int count = dataGridViewListOfStudents.Rows.Count;
+
+            if (count != 0)
+            {
+                int min = Convert.ToInt32(dataGridViewListOfStudents.Rows[0].Cells[3].Value);
+
+                if (count < 1)
+                {
+                    for (int i = 1; i > dataGridViewListOfStudents.Rows.Count; i++)
+                    {
+                        int rating = Convert.ToInt32(dataGridViewListOfStudents.Rows[i].Cells[3].Value);
+                        if (min < rating)
+                        {
+                            min = rating;
+                        }
+                    }
+
+
+
+                }
+
+
+                textBoxMax.Text = min.ToString();
+
+
+            }
+        }
+    }
+}
